284fe3f8aa84bd158bb9360df4587b7d56921337
jiangrui <>
2024-01-17 18:30:39 +0800

    【web】【issue_id=xxx】命名空间组件 使用方法二

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -6,13 +6,13 @@ import Label from './Label.vue'
 // 命名空间组件 使用方法一
 // 使用点语法来使用挂载在一个对象上的组件
 // 把Input、Label组件挂载到 Form 组件上
-Form.Input = Input
-Form.Label = Label
-export default Form
+// Form.Input = Input
+// Form.Label = Label
+// export default Form
 
 // 命名空间组件 使用方法二
-// export default {
-//     // Form,
-//     Input,
-//     Label,
-// }
+export {
+    Form,
+    Input,
+    Label,
+}
--- src/views/NamespaceView.vue
+++ src/views/NamespaceView.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
 // 使用一
-import Form from '../components/Form/index'
-// import * as Form from '../components/Form/index'
+// import Form from '../components/Form/index'
 
 
+// 使用二
+import * as Form from '../components/Form/index'
+console.log(Form)
 </script>
 
 <template>
     <!-- 使用一 -->
-  <Form>
+  <!-- <Form>
     <Form.Label />
     <Form.Input />
-  </Form>
+  </Form> -->
 
   <!-- 使用二 -->
-
+  <Form.Form>
+    <Form.Label />
+    <Form.Input />
+  </Form.Form>
 </template>
